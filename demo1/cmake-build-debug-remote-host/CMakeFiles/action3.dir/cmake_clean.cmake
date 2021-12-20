6ee8adc5d3e87b1fd36ff5dd0997b1efba0e88ff
zzj <912290893@qq.com>
2021-12-21 01:05:25 +0800

    asdf

---
+++ demo1/cmake-build-debug-remote-host/CMakeFiles/action3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/action3.dir/main.cpp.o"
+  "CMakeFiles/action3.dir/main.cpp.o.d"
+  "action3"
+  "action3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/action3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ demo1/cmake-build-debug/CMakeFiles/action3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/action3.dir/main.cpp.obj"
+  "action3.exe"
+  "action3.exe.manifest"
+  "action3.pdb"
+  "libaction3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/action3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
